6cdc7002b60756164305893cda5dc38b3ddb34f7
Baut Antoine <bauta@classes.local>
2024-01-16 09:27:05 +0100

    Creation Project

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+const HomePage = (props) => {
+    return ( 
+        <div className="jumbotron">
+            <h1 className="display-3">Hello World!</h1>
+            <p className="load">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, eum amet fugit beatae rem consequatur illum aspernatur quas quaerat molestias ut perspiciatis, nisi harum accusantium. Porro enim sint ratione quae!</p>
+            <hr className="my-4" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, aut?</p>
+            <p className="lead">
+                <a href="#" className="btn btn-primary btn-lg">Learn More</a>
+            </p>
+        </div>
+     );
+}
+ 
+export default HomePage;
